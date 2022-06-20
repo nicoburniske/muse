f3bb0d8861bcf53993a858fc4f88844bcd235a52
Nico Burniske <nicoburniske@gmail.com>
2022-06-20 11:00:52 -0400

    integrated spotify entities but experiencing silent failure

--- src/main/scala/muse/graphql/ReviewEntity.scala
+++ src/main/scala/muse/graphql/ReviewEntity.scala
@@ -6,15 +6,18 @@ import muse.domain.common.EntityType
 import muse.domain.spotify.AlbumType
 import muse.domain.tables.ReviewComment
 import muse.service.persist.{DatabaseQueries, QuillContext}
-import muse.service.spotify.SpotifyAPI
+import muse.service.spotify.{SpotifyAPI, SpotifyService}
+import sttp.client3.asynchttpclient.zio.AsyncHttpClientZioBackend
 import zio.Chunk
 import zio.*
 import zio.Console.printLine
 import zio.query.{CompletedRequestMap, DataSource, Request, ZQuery}
+import caliban.wrappers.Wrappers.printErrors
 
 import java.sql.SQLException
 import java.time.Instant
 import java.util.UUID
+import scala.util.Try
 
 // TODO: consider if ids or something simple should exist outside as field outside of sub-entity.
 
@@ -45,7 +48,10 @@ object Reqs {
             tReview.reviewName,
             tReview.isPublic,
             //            _ => getReviewComments(tReview.id))
-            getReviewComments(tReview.id)
+            getReviewComments(tReview.id),
+            tReview.entityId,
+            tReview.entityType,
+            getEntity(tReview.entityId, tReview.entityType)
           )
         }
       }
@@ -59,7 +65,17 @@ object Reqs {
     DataSource.fromFunctionZIO("ReviewDataSource") { g =>
       DatabaseQueries.getReview(g.reviewId).map {
         _.map { r =>
-          Review(r.id, r.createdAt, r.creatorId, r.reviewName, r.isPublic, getReviewComments(r.id))
+          Review(
+            r.id,
+            r.createdAt,
+            r.creatorId,
+            r.reviewName,
+            r.isPublic,
+            getReviewComments(r.id),
+            r.entityId,
+            r.entityType,
+            getEntity(r.entityId, r.entityType)
+          )
         }
       }
     }
@@ -78,8 +94,13 @@ object Reqs {
       r.commenter,
       getUser(r.commenter),
       r.comment,
-      r.rating)
+      r.rating,
+      r.entityId,
+      r.entityType,
+      getEntity(r.entityId, r.entityType)
+    )
 
+  // This could be weird on second thought because of permissions.
   val CommentDataSource: DataSource[DatabaseQueries, GetReviewComments] =
     DataSource.Batched.make("ReviewCommentsDataSource") { (requests: Chunk[GetReviewComments]) =>
       requests.toList match
@@ -124,38 +145,77 @@ case class Queries(
     reviews: ReviewsArgs => ZQuery[DatabaseQueries, Nothing, Option[Review]])
 
 object TestMain extends ZIOAppDefault {
-  given userSchema: Schema[DatabaseQueries, User] = Schema.gen
+  //  given pagSchema: Schema[DatabaseQueries, Pagination]   = Schema.genDebug
+  given userSchema: Schema[DatabaseQueries & SpotifyAPI[Task], User] = Schema.gen
 
   given userArgs: Schema[DatabaseQueries, UserArgs] = Schema.gen
 
-  //  given pagSchema: Schema[DatabaseQueries, Pagination]   = Schema.genDebug
-  given reviewSchema: Schema[DatabaseQueries, Review] = Schema.gen
+  given reviewSchema: Schema[DatabaseQueries & SpotifyAPI[Task], Review] = Schema.gen
 
-  given commentsSchema: Schema[DatabaseQueries, Comment] = Schema.gen
+  given commentsSchema: Schema[DatabaseQueries & SpotifyAPI[Task], Comment] = Schema.gen
+
+  given entitySchema: Schema[SpotifyAPI[Task], ReviewEntity] = Schema.gen
+
+  given albumSchema: Schema[SpotifyAPI[Task], Album] = Schema.gen
+
+  given artistSchmea: Schema[SpotifyAPI[Task], Artist] = Schema.gen
+
+  given playlistSchema: Schema[SpotifyAPI[Task], Playlist] = Schema.gen
+
+  given playlistTrackSchema: Schema[SpotifyAPI[Task], PlaylistTrack] = Schema.gen
+
+  given trackSchema: Schema[SpotifyAPI[Task], Track] = Schema.gen
 
   val resolver = Queries(id => Reqs.getUser(id.id), ids => Reqs.getReview(ids.id))
-  val api      = GraphQL.graphQL[DatabaseQueries, Queries, Unit, Unit](RootResolver(resolver))
+  val api      =
+    GraphQL.graphQL[DatabaseQueries & SpotifyAPI[Task], Queries, Unit, Unit](
+      RootResolver(resolver)) @@ printErrors
+
   println(api.render)
-  val query    =
+
+  val query =
     """
     query {user(id: "tuckingfypo1") {
       reviews {
         id
         reviewName
         isPublic
+        entityId
+        entityType
+        entity {  
+          ... on Track {
+            name
+          }
+        }
        }
     }
     }""".stripMargin
 
-  val dbLayer = QuillContext.dataSourceLayer >>> DatabaseQueries.live
+  val dbLayer     = QuillContext.dataSourceLayer >>> DatabaseQueries.live
+  val accessToken =
+    "BQCsw_Ka4bIJnobWfo3voew-9eSvb-vFhDaWtzI7y7vbywrqYX6rJAxwkivdXoOu1h0-DF2Q563MQyXOFwvtDiOMImJXZ6cV34sruX97FWL3-IDIa6uR70ppH662ikNsloBm39wRsz4GgUqNFTbmwIwNmrnw_w6XL1Z9-O9-7xBSrvd6YTkaa76IuPeE5gZboBE"
+
+  import muse.utils.Givens.given
+
+  val spotifyLayer = AsyncHttpClientZioBackend.layer() >>> ZLayer.fromZIO(SpotifyService.live(accessToken))
 
   override def run =
     api
       .interpreter
-      .flatMap(_.execute(query))
-      .flatMap(r => printLine(r.toString))
+      .flatMap { interpreter =>
+        for {
+          _   <- ZIO.logInfo("Doing something!")
+          res <- ZIO.fromTry(Try(interpreter.execute(query))).flatMap(identity)
+          _   <- ZIO.logInfo(s"Succeed! ${res.toString}")
+        } yield res
+      }
+      .flatMap { r =>
+        // comments
+        ZIO.logInfo("IN HERE") *> ZIO.logInfo(r.toString)
+      }
+      .catchAll { e => ZIO.logInfo("OH NO AN ERROR") *> ZIO.logError(e.getMessage) }
       .exitCode
-      .provideLayer(dbLayer)
+      .provideLayer(dbLayer ++ spotifyLayer)
 }
 
 // Responses
@@ -177,8 +237,10 @@ final case class Review(
     reviewName: String,
     isPublic: Boolean,
     //    comments: Pagination => ZQuery[DatabaseQueries, Nothing, List[Comment]]
-    comments: ZQuery[DatabaseQueries, Nothing, List[Comment]]
-    // ,entity: ZQuery[Any, Nothing, ReviewEntity]
+    comments: ZQuery[DatabaseQueries, Nothing, List[Comment]],
+    entityId: String,
+    entityType: EntityType,
+    entity: ZQuery[SpotifyAPI[Task], Throwable, ReviewEntity]
 )
 
 final case class Comment(
@@ -188,44 +250,45 @@ final case class Comment(
     updatedAt: Instant,
     // If none, then it is root comment.
     parentCommentId: Option[Int],
-    // Should the user comment ID be available by default? Should there be a parameter here?
     commenterId: String,
     commenter: ZQuery[DatabaseQueries, Nothing, User],
     comment: Option[String],
-    rating: Option[Int]
-    // , entity: ZQuery[Any, Nothing, User]
+    rating: Option[Int],
+    entityId: String,
+    entityType: EntityType,
+    entity: ZQuery[EntityEnv, Throwable, ReviewEntity]
 )
 
-//case class GetUser(id: String) extends Request[Nothing, User]
-//def getUser(id: String)                                  = ZQuery.fromRequest(GetUser(id))(UserDataSource)
-//val UserDataSource: DataSource[DatabaseQueries, GetUser] =
-//  DataSource.fromFunction("UserDataSource")(req => User(req.id, getUserReviews(req.id)))
+def getEntity(entityId: String, entityType: EntityType): ZQuery[EntityEnv, Throwable, ReviewEntity] =
+  entityType match
+    case EntityType.Album    => getAlbums(List(entityId)).map(_.head)
+    case EntityType.Artist   => getArtists(List(entityId)).map(_.head)
+    case EntityType.Playlist => getPlaylist(entityId)
+    case EntityType.Track    => getTracks(List(entityId)).map(_.head)
 
 type EntityEnv = SpotifyAPI[Task]
 
 case class GetAlbums(ids: List[String]) extends Request[Throwable, List[Album]]
 
-def getAlbums(albumIds: List[String])                 = ZQuery.fromRequest(GetAlbums(albumIds))(AlbumDataSource)
-val AlbumDataSource: DataSource[EntityEnv, GetAlbums] = DataSource.fromFunctionZIO("AlbumDataSource") {
+def getAlbums(albumIds: List[String])                     = ZQuery.fromRequest(GetAlbums(albumIds))(AlbumDataSource)
+val AlbumDataSource: DataSource[EntityEnv, GetAlbums]     = DataSource.fromFunctionZIO("AlbumDataSource") {
   reqs =>
     ZIO.service[SpotifyAPI[Task]].flatMap { spotify =>
-      spotify.getAlbums(reqs.ids).map {
-        _.map { a =>
-          Album(
-            a.albumGroup,
-            a.albumType.toString,
-            a.externalUrls,
-            a.genres.getOrElse(Nil),
-            a.id,
-            a.images.map(_.url),
-            a.label,
-            a.name,
-            a.popularity,
-            a.releaseDate)
-        }.toList
-      }
+      spotify.getAlbums(reqs.ids).map { albums => albums.map(spotAlbumToAlbum).toList }
     }
 }
+def spotAlbumToAlbum(a: muse.domain.spotify.Album): Album =
+  Album(
+    a.albumGroup,
+    a.albumType.toString,
+    a.externalUrls,
+    a.genres.getOrElse(Nil),
+    a.id,
+    a.images.map(_.url),
+    a.label,
+    a.name,
+    a.popularity,
+    a.releaseDate)
 
 sealed trait ReviewEntity
 
@@ -235,7 +298,6 @@ case class Album(
     externalUrls: Map[String, String],
     genres: List[String],
     id: String,
-    // Sort them largest to smallest?
     images: List[String],
     label: Option[String],
     name: String,
@@ -253,26 +315,32 @@ def getTracks(albumIds: List[String])                 = ZQuery.fromRequest(GetTr
 val TrackDataSource: DataSource[EntityEnv, GetTracks] = DataSource.fromFunctionZIO("TrackDataSource") {
   reqs =>
     ZIO.service[SpotifyAPI[Task]].flatMap { spotify =>
-      spotify.getTracks(reqs.ids).map {
-        _.map { t =>
-          Track(
-            getAlbums(List(t.album.get.id)).map(_.head),
-            t.discNumber,
-            t.durationMs,
-            t.explicit,
-            t.externalUrls,
-            t.href,
-            t.id,
-            t.isPlayable.get,
-            t.name,
-            t.popularity.get,
-            t.previewUrl.get,
-            t.trackNumber,
-            t.isLocal,
-            t.uri
-          )
-        }.toList
-      }
+      spotify
+        .getTracks(reqs.ids)
+        .map {
+          _.map { t =>
+            Track(
+              getAlbums(List(t.album.get.id)).map(_.head),
+              t.discNumber,
+              t.durationMs,
+              t.explicit,
+              t.externalUrls,
+              t.href,
+              t.id,
+              t.isPlayable.get,
+              t.name,
+              t.popularity.get,
+              t.previewUrl.get,
+              t.trackNumber,
+              t.isLocal,
+              t.uri
+            )
+          }.toList
+        }
+        .tapBoth(
+          e => ZIO.logError("OH NO AN ERROR!" + e.getMessage),
+          t => ZIO.logInfo(s"${t.size} Tracks retrieved successfully")
+        )
     }
 }
 
